ffed86e225390600e4d1d625c1c5955791bf5424
FITOUSSI Emmanuel <emmanuel.fitoussi@wit.fr>
2021-04-05 21:26:40 +0200

    script-api

---
+++ script-api/api.js
@@ -0,0 +1,43 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "Actor.BaseActor",
+        "Actor.DataSource",
+        "Actor.DataSource.WosRelativeVariableSource",
+        "Actor.DataSource.WosVariableSource",
+        "Actor.Display.Html",
+        "Actor.Event.BaseContext",
+        "Actor.Event.PropertyChangedContext",
+        "BaseHost",
+        "BaseSession",
+        "Behavior.Actor.Jokerable",
+        "Behavior.DataContextual",
+        "Behavior.Destroyable",
+        "Binding.BasicBinding",
+        "Binding.InternalBinding",
+        "Binding.Setup.Actor",
+        "Binding.Setup.Basic",
+        "Binding.Setup.Internal",
+        "Binding.Setup.Relative",
+        "Binding.Setup.Stage",
+        "RedyHost",
+        "RedySession"
+    ],
+    "modules": [
+        "Synapps-UI",
+        "Synapps-UI-REDY"
+    ],
+    "allModules": [
+        {
+            "displayName": "Synapps-UI",
+            "name": "Synapps-UI"
+        },
+        {
+            "displayName": "Synapps-UI-REDY",
+            "name": "Synapps-UI-REDY",
+            "description": "Représente l'ULI hôte."
+        }
+    ],
+    "elements": []
+} };
+});
